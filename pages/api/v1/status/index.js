eeaaee2b2dd10bac0f595d432e380936f20e7b4c
Roch <tayronroch@gmail.com>
2025-12-18 12:50:48 -0300

    add jsconfig.json file and some settings

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,6 @@
-import database from "../../../../infra/database/index.js";
+import database from "infra/database.js";
 
-async function status(requert, response) {
+async function status(request, response) {
   const result = await database.query("SELECT 1 as status");
   console.log(result);
   response.status(200).json({ status: "São acima da Média" });
