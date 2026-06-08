da5d12134b95c378fead1b35b2e1e5989c86e723
Tayron Rocha <tayronroch@gmail.com>
2026-06-08 12:51:22 -0300

    chore: Adds `wait-for-postgres`script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,20 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec(
+    "docker exec clone-tabnews-db pg_isready --host localhost",
+    handleResult
+  );
+
+  function handleResult(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+    console.log("\n\n🟢 PostgreSQL está pronto para conexões!");
+  }
+}
+
+process.stdout.write("\n\n🔴 Aguardando o PostgreSQL aceitar conexões...");
+checkPostgres();
