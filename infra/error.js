3577625c572f0d0e17d66fc319bed862517baa3e
Roch <tayronroch@gmail.com>
2026-06-16 21:38:45 -0300

    feat: add `MethodNotAllowedError` and `ServiceError`

--- infra/error.js
+++ infra/error.js
@@ -1,5 +1,5 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause } = {}) {
     super("Um erro interno não esperado aconteceu.", {
       cause,
     });
@@ -9,6 +9,33 @@ export class InternalServerError extends Error {
     this.action = "Entre em contato com o suporte.";
 
     this.statusCode = 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+
+      message: this.message,
+
+      action: this.action,
+
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor({ cause, message } = {}) {
+    super(message || "The HTTP method is not allowed for this endpoint.", {
+      cause,
+    });
+
+    this.name = "MethodNotAllowedError";
+
+    this.action =
+      "Verify to be method is correct and supported by this endpoint.";
+
+    this.statusCode = 405;
   }
 
   toJSON() {
